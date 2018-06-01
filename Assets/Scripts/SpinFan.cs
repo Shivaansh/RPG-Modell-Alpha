c804e69244acc386c29092f58309a71c391cb4fd
Shivaansh Prasann <shivaanshseal@gmail.com>
2018-06-01 19:45:10 +0530

    Player House area designed, mountains added to terrain, PlayerHouse prefab created

--- Assets/Scripts/SpinFan.cs
+++ Assets/Scripts/SpinFan.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpinFan : MonoBehaviour {
 
-
+    public int spinDegrees;
     //this class was first used to spin the fan on the windmill
     //change if applied to the prefab will cause the fan to spin 25 degrees per second
 	// Use this for initialization
@@ -16,7 +16,7 @@ public class SpinFan : MonoBehaviour {
 	void Update () {
 
         // Rotate the object around its local Z axis at 1 degree per second
-        transform.Rotate(Vector3.forward * Time.deltaTime * 25);
+        transform.Rotate(Vector3.forward * Time.deltaTime * spinDegrees);
 
         
     }
