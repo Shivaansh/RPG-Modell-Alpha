ed8341aeed0c0ae6dafb663a731b1924300da137
Shivaansh Prasann <shivaanshseal@gmail.com>
2018-06-14 17:03:36 +0530

    Click to Attack implemented
    Clicking on enemies (while in range) attacks the enemies. Animation STILL TO BE ADDED.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,49 @@ public class Player : MonoBehaviour, IDamageablePlayer {
 
     //maximum player health
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] int enemyLayer = 9;
+    [SerializeField] float playerDamagePerHit = 90f;
+    [SerializeField] float timeBetnhits = 1f;
+    [SerializeField] float meleeRange = 2f;
+
+    float lastHitTime = 0f;
+
+    GameObject currentTarget; //reference to enemy
+    CameraRaycaster camCaster;
 
     //current health level
-    private float currentHealthPoints = 100f;
+    private float currentHealthPoints;
+
+
+    void Start()
+    {
+        camCaster = FindObjectOfType<CameraRaycaster>();
+        camCaster.notifyMouseClickObservers += onMouseClick;
+        currentHealthPoints = maxHealthPoints;
+    }
+
+     void onMouseClick(RaycastHit raycastHit, int layerHit)
+    {
+        if (layerHit == enemyLayer) //if the layer hit by ray has same index as enemy layer
+        {
+            var enemy = raycastHit.collider.gameObject; //enemy now holds the enemy gameObject
+            //print("Clicked on enemy");
+            currentTarget = enemy;
+            float attackDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            var enemyComponent = enemy.GetComponent<Enemy>();
+            
+           
+            if (Time.time - lastHitTime > timeBetnhits && attackDistance <= meleeRange)
+            {
+                enemyComponent.TakeDamage(playerDamagePerHit);
+                lastHitTime = Time.time;
+            }
+        }
+    }
 
     /**
-     * @brief: getter method to return current health level as a value between 0 and 1
-     */
+* @brief: getter method to return current health level as a value between 0 and 1
+*/
     public float getHealthAsPercentage { get { return currentHealthPoints / (float)maxHealthPoints; } }
 
     //Makes player susceptible to damage
@@ -24,5 +61,7 @@ public class Player : MonoBehaviour, IDamageablePlayer {
         if (currentHealthPoints <= 0) { Destroy(gameObject); } //kills the player when health reached 0
         //TODO: add a load level statement to load death scene.
      //this  = Player
-}
+     }
+
+    //TODO : maybe make a regenerating method?
 }
