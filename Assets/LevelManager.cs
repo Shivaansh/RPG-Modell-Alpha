e144b41034554090fb37bd667c2f293da23b349f
Shivaansh Prasann <shivaanshseal@gmail.com>
2018-09-22 19:51:01 -0400

    Start Scene Added
    Added a start scene with background image and music

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {//public because it needs to be called outside the class as well
+        Debug.Log("Level Load requested for: " + name);
+        //HitCount.brickCount=0;
+        Application.LoadLevel(name);
+
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        //HitCount.brickCount=0;
+        Application.LoadLevel(Application.loadedLevel + 1);
+
+    }
+
+    public void BrickDestroyed()
+    {
+        //if(HitCount.brickCount<=0){
+        LoadNextLevel();
+
+    }
+}
+
